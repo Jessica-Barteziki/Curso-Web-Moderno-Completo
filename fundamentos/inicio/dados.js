9d925b72f44bd3a9160950ca036d2b276c3c2380
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 11:18:29 -0300

    Update dados js

--- fundamentos/inicio/dados.js
+++ fundamentos/inicio/dados.js
@@ -2,8 +2,8 @@
 var nome = "Caneta";
 var quantidade = 10;
 var preco = 6.4;
-var imposto = 1.5;
-var precoFinal = preco + imposto;
+let imposto = 1.5; //mais utilizado atualmente
+let precoFinal = preco + imposto;
 
 console.log(nome);
 console.log(quantidade);
