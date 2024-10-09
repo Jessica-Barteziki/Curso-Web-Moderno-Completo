6557bd0abf90296e38db43e302d9151b6555eedd
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 14:11:25 -0300

    Create desafio_area.js

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,10 @@
+//formula para calcular pi * raio * raio
+
+let raio = 10;
+const pi = 3.141592;
+let areaCirc = pi * (raio * raio);
+
+console.log("O valor da área é " + areaCirc + "m2");
+
+areaCirc =  Math.PI * (raio * 2);
+console.log("O valor da área é " + areaCirc + "m2")
