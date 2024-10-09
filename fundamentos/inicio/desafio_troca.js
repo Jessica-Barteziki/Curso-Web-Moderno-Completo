5d13f1071e5876fa52183758d8c9caf7b79cdcef
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 14:26:20 -0300

    Create desafio_troca js

---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,16 @@
+let a = 7;
+let b = 94;
+//let temp = a;  //variável temporária
+
+
+// a = b;
+// b = temp;
+// console.log(a);
+// console.log(b);
+
+//outra forma de fazer:
+
+[a, b] = [b, a]
+
+console.log(a);
+console.log(b);
