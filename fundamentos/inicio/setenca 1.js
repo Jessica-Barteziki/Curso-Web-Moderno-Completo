b5a55a075e39dd88180ed300e904966eae402fa7
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 10:52:33 -0300

    Update setenca 1 js

--- fundamentos/inicio/setenca 1.js
+++ fundamentos/inicio/setenca 1.js
@@ -2,3 +2,5 @@ console.log("Olá, Mundo!!!");
 console.log("Bom dia");
 console.log("Boa tarde");
 console.log("Boa noite");
+
+//as sentenças são executadas em ordem do inicio do código para o final
