f1632c15a35e13ad08e09341f874b8ac6ba4d9d2
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 11:32:01 -0300

    Update dados_2 js

--- fundamentos/inicio/dados_2.js
+++ fundamentos/inicio/dados_2.js
@@ -4,3 +4,11 @@ let desconto = 0.4;
 console.log(preco * 0.6);
 let precoComDesconto = preco * (1 - desconto);
 console.log(precoComDesconto);
+
+let nome = "Caderno"; // string (texto) -> sequencia de símbolos 
+let categoria = "Papelaria";
+console.log("Produto: " + nome
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto
+)
