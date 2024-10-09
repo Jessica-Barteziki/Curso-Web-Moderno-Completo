8aaf3eb6d0d9507491a38fc3df058e783a320d91
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 15:19:01 -0300

    Update organizacao js

--- exercicios-js/fundamentos/organizacao.js
+++ exercicios-js/fundamentos/organizacao.js
@@ -1,8 +1,12 @@
-/* o js é organizada em linhas e blocos de códigos 
+/* o js é organizado em sentença de código que podem ou não terminar com ";" e também blocos de códigos que são delimitados "{}"
 */
 
 console.log("Sentença de código");
 
 {
-    //delimitador bloco js
+    {
+        //delimitador bloco js
+        console.log("Olá");
+        console.log("Mundo!"); // padrão do curso
+    }
 }
