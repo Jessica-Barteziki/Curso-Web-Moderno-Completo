e42fc91dabaf03e0915a64d910965d968d2926e5
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 16:37:47 -0300

    Update numerosAlgunsCuidados js

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -4,4 +4,4 @@ console.log("Show" * 2);
 console.log(0.1 + 0.7);
 // console.log(10.toString()); não consegue chamar o diretamente o literal. a função
 console.log((10.345).toFixed(2)); // mas consegue chamar o literal dentro de parenteses e chamar a função
-console.log('3' + 2)
+console.log('3' + 2) // a string ganha 
