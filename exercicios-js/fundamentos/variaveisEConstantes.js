eaeed713e48996ae6d0f0974887ba20d31d13f43
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-09 15:47:30 -0300

    Update variaveisEConstantes js

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,11 @@
+var a = 3;
+let b = 4;
+
+var a = 300;
+ b = 400;
+
+console.log(a, b);
+
+const c = 5; //constante, tudo o que não precisar mudar deve dar preferencia para constante
+// c = 50;
+console.log(c)
