d4e44365282cc5a94b117f98427fef0c46f716b9
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-10 08:25:38 -0300

    Create hoisting js

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,21 @@
+console.log('a = ', a)
+var a = 2
+console.log('a = ', a)
+
+/*dentro do bloco também acontece o issamento
+function teste() {
+    console.log('b = ', b)
+    var b = 4
+    console.log('b = ', b)
+}
+
+teste()
+*/
+/* -- no let não acontece como o var, então deve ter cuidado
+console.log('b = ', b)
+let b = 5
+console.log('b = ', b)
+*/
+
+let b = 5
+console.log('b = ',b)
