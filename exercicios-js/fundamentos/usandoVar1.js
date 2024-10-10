39c5b47c5a19da2b9624e7557e5c75444acc0219
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-10 07:59:17 -0300

    Summary (required)

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            { var sera = 'Será?????' }
+        }
+    }
+}
+console.log(sera);
+function teste(){
+    var local = 123;
+    console.log(local)
+}
+
+teste();
+console.log(local)
+
+//existe somente duas formas de utilizar var: de forma global visivel para todo mundo ou dentro de um escopo função (melhor utilizar dentro de um escopo para não dar problema)
