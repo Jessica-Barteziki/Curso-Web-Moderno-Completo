4c6caa1dfdba529d5fb5a1ef3ccdf6a796e4fb00
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-10 15:16:39 -0300

    Create breakContinue js

---
+++ exercicios-JS/Controle/breakContinue.js
@@ -0,0 +1,22 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (x in nums) {
+    if (x == 5) {
+        break // ele sai do lado corrente
+    }
+    console.log(`${x}= ${nums[x]}`)
+}
+for (y in nums) {
+    if (y == 5) {
+        continue //interrompe e vai para a proxima repetição. só faz sentido agir em cima de repetição 
+    }
+    console.log(`${y} = ${nums[y]}`)
+}
+
+externo:
+for (a in nums) {
+    for (b in nums) {
+        if (a == 2 && b == 3) break externo
+        console.log(`Par = ${a}, ${b}`)
+    }
+}
