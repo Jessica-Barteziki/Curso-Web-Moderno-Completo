04b6c27ba43b4be6c2a717c3703a614caeaa7b0b
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-11 14:09:38 -0300

    Update arrowFunction3 js

--- exercicios-JS/Função/arrowFunction3.js
+++ exercicios-JS/Função/arrowFunction3.js
@@ -3,3 +3,11 @@ let comparaComThis = function (param){
 }
 
 comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+//Por conta do bind o this aponta para objeto dentro dessa função
+
