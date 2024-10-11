932ac30fe9ff5a08d0bd2b28f6d30dbd0764f22c
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-11 14:17:33 -0300

    Update arrowFunction3 js

--- exercicios-JS/Função/arrowFunction3.js
+++ exercicios-JS/Função/arrowFunction3.js
@@ -1,3 +1,4 @@
+//this de uma função arrow não vai variar, ele é  associado ao contexto no qual a função foi escrita
 let comparaComThis = function (param){
     console.log(this === param)
 }
@@ -11,3 +12,12 @@ comparaComThis(obj)
 
 //Por conta do bind o this aponta para objeto dentro dessa função
 
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+
+//utilizado bind dentro da função arrow
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
