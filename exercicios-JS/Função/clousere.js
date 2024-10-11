d1bb1e1dc255bda88f8bd59ed31ceac06148ede5
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-11 18:04:17 -0300

    Update clousere js

---
+++ exercicios-JS/Função/clousere.js
@@ -0,0 +1,18 @@
+/* Clousere é o escopo criado quando uma função é decladara 
+Esse escopo permite a função acessar e manipular variáveis externas à função
+
+Contexto Lexico em ação!
+*/
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro (){
+        return x 
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
