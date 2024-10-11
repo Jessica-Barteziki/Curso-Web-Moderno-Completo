d1566cd55cac49165281b367401892835f616f6c
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-11 08:58:00 -0300

    Update paramsPadrao js

--- exercicios-JS/Função/paramsPadrao.js
+++ exercicios-JS/Função/paramsPadrao.js
@@ -12,7 +12,7 @@ console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
 function soma2(a, b, c){
     a = a !== undefined ? a : 1
     b = 1 in arguments ? a : 1
-    c = isNaN(c) ? 1 : c
+    c = isNaN(c) ? 1 : c // versão mais segura, caso não utilize o valor padrão es2015
     return a + b + c
 }
 console.log(soma2(), soma2(3), soma2(1,2, 3), soma2(0, 0, 0))
