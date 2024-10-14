703d6997416ab9f1b0c95af9e9f834f2b1eb06a7
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-14 08:14:38 -0300

    Update iife js

--- exercicios-JS/Função/iife.js
+++ exercicios-JS/Função/iife.js
@@ -1,2 +1,7 @@
-// IIFE => Immediately Invoked Function Expression - Expressão de função invocada imediatamente
-()
+/*IIFE => Immediately Invoked Function Expression - Expressão de função invocada imediatamente
+Para fugir do escopo global é utilizado a função auto-invocada para não haver problemas com as aplicações no browser. 
+*/
+(function(){
+    console.log('Será executado na hora!')
+    console.log ('Foge do escopo mais abrangente!')
+})()
