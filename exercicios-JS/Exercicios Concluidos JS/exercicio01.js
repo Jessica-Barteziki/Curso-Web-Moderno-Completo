e9d31d9d4757c944a06f0e56ed390e9642d54f34
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-14 08:43:09 -0300

    Update exercicio01 js

---
+++ exercicios-JS/Exercicios Concluidos JS/exercicio01.js
@@ -0,0 +1,10 @@
+a = 10
+b = 15
+
+const soma  = a + b 
+const subtracao = b - a
+const multiplicacao = a * b
+const divisao = b / a
+const modulo = b % a
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
