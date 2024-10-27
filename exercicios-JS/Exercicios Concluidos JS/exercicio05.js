b79eaefc96952f6886ed488f3eb7144e7f1891bd
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-27 09:49:24 -0300

    Update teste

--- exercicios-JS/Exercicios Concluidos JS/exercicio05.js
+++ exercicios-JS/Exercicios Concluidos JS/exercicio05.js
@@ -2,5 +2,6 @@ function formatarValorDecimal(valorDecimal) {
     valorEmReais = `R$ ${valorDecimal.toFixed(2).toString().replace(".", ",")}`
     console.log(valorEmReais)
 }
+ gvbgbn gfc
 
 formatarValorDecimal(0.1 + 4.8)
