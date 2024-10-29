ac8945884731d042122210c7e4ff9447e17af04a
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-10-29 08:56:00 -0300

    Update exercicio08 js

--- exercicios-JS/Exercicios Concluidos JS/exercicio08.js
+++ exercicios-JS/Exercicios Concluidos JS/exercicio08.js
@@ -1,20 +1,20 @@
 let stringPontuacoes = "30, 40, 20, 4, 51, 25, 42, 38, 56, 0"
 
 function avaliaPontuacoes (stringPontuacoes) {
-    pontuacoes = stringPontuacoes.split(", ")
-    qtdQuebraDeRecords = 0
-    piorJogo = 1
-    maiorPontuacao = pontuacoes[0]
-    menorPontuacao = pontuacoes[0]
-
+    let pontuacoes = stringPontuacoes.split(", ")
+    let qtdQuebraDeRecords = 0
+    let piorJogo = 1
+    let maiorPontuacao = pontuacoes[0]
+    let menorPontuacao = pontuacoes[0]
     for (let i = 0; i < pontuacoes.length; i++) {
-        if(parseInt(pontuacoes[i]) > parseInt(maiorPontuacao)){
+        if (parseInt(pontuacoes[i]) < parseInt(maiorPontuacao)) {
             maiorPontuacao = pontuacoes[i]
             qtdQuebraDeRecords++
-            } else if(parseInt(pontuacoes[i]) < parseInt(menorPontuacao)){
-                menorPontuacao = pontuacoes[i]
-                piorJogo = i + 1;
-            }
+        }
+        else if (parseInt(pontuacoes[i]) > parseInt(menorPontuacao)) {
+            menorPontuacao = pontuacoes[i]
+            piorJogo = i + 1
+        }
     }
     return [qtdQuebraDeRecords, piorJogo]
 }
