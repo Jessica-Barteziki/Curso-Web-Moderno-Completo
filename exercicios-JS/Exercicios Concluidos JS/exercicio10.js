23b27b2fb702e8e21a2266144c6899b7c3e04418
Jéssica Barteziki <jessicabarteziki@gmail.com>
2024-11-22 18:55:42 -0300

    Update exercicio10 js

---
+++ exercicios-JS/Exercicios Concluidos JS/exercicio10.js
@@ -0,0 +1,12 @@
+function verificaNumero (numero) {
+    if (numero % 3 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(verificaNumero(3))
+console.log(verificaNumero(8))
+console.log(verificaNumero(9))
+console.log(verificaNumero(10))
